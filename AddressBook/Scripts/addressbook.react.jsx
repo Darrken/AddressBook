f0da0d40ed334bdf1fc0f4240d1c0b7c0188c7d4
Ken <ken.james.adams@gmail.com>
2016-02-29 11:29:13 -0700

    styling with bootstrap for speed

--- AddressBook/Scripts/addressbook.react.jsx
+++ AddressBook/Scripts/addressbook.react.jsx
@@ -1,13 +1,16 @@
 var SearchPanel = React.createClass({
     render: function() {
 	    return (
-		    <div className="row">
-                <div className="one-fourth column">
-                    Filter: &nbsp;
-                    <input ref="search" type="text" value={this.props.search} onChange={this.onSearchChanged} />
-                    {this.props.search ? <button onClick={this.props.onClearSearch} >x</button> : null}
-                </div>
-            </div>
+            <form className="form-inline">
+				<div className="form-group">
+					<label className="sr-only" htmlFor="filter">Filter</label>
+					<div className="input-group">
+						<div className="input-group-addon"><span className="glyphicon glyphicon-filter" aria-hidden="true"></span></div>
+						<input ref="search" name="filter" type="text" value={this.props.search} onChange={this.onSearchChanged} className="form-control" />
+						{this.props.search ? <div className="input-group-addon" onClick={this.props.onClearSearch }><span className="glyphicon glyphicon-remove" aria-hidden="true"></span></div> : null}
+					</div>
+				</div>
+            </form>
 	    );
     },
     onSearchChanged: function() {
@@ -39,18 +42,21 @@ var ContactTable = React.createClass({
         this.props.contacts.forEach(function(contact) {
             rows.push(<ContactTableRow key={contact.id} contact={contact} handleEditClickPanel={this.props.handleEditClickPanel} />);
         }.bind(this));
-        return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-						<th>Phone</th>
-                        <th>Address</th>
-                    </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-            </table>
+    	return (
+			<div className="table-responsive">
+				<table className="table table-hover">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Phone</th>
+							<th>Address</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>{rows}</tbody>
+				</table>
+			</div>
         );
     }
 });
@@ -59,16 +65,31 @@ var ContactForm = React.createClass({
     render: function() {
         return(
             <form onSubmit={this.props.handleSubmitClick}>
-                <label forHtml="firstName">First Name</label><input ref="firstName" name="firstName" type="text" value={this.props.contact.firstName} maxLength="15" required onChange={this.onChange}/>
-                <label forHtml="lastName">Last Name</label><input ref="lastName" name="lastName" type="text" value={this.props.contact.lastName} maxLength="15" required onChange={this.onChange}/>
-                <label forHtml="email">Email</label><input ref="email" name="email" type="email" value={this.props.contact.email}  maxLength="30" required onChange={this.onChange}/>
-                <label forHtml="phone">Phone</label><input ref="phone" name="phone" type="tel" value={this.props.contact.phone} maxLength="15" onChange={this.onChange}/>
-                <label forHtml="address">Address</label><input ref="address" name="address" type="text" value={this.props.contact.address} maxLength="50" onChange={this.onChange}/>
-                <input type="submit" value={this.props.contact.id ? "Save (id = " + this.props.contact.id + ")" : "Add"} />
-                {this.props.contact.id ? <button onClick={this.props.handleDeleteClick}>Delete</button> : null}
-                {this.props.contact.id ? <button onClick={this.props.handleCancelClick}>Cancel</button> : null}
-                {this.props.message ? <div>{this.props.message}</div> : null}
-            </form>
+				<div className="form-group">
+					<label forHtml="firstName">First Name</label>
+					<input ref="firstName" name="firstName" type="text" value={this.props.contact.firstName} maxLength="15" required onChange={this.onChange} className="form-control" />
+				</div>
+				<div className="form-group">
+	               <label forHtml="lastName">Last Name</label>
+					<input ref="lastName" name="lastName" type="text" value={this.props.contact.lastName} maxLength="15" required onChange={this.onChange} className="form-control" />
+				</div>
+				<div className="form-group">
+					<label forHtml="email">Email</label>
+					<input ref="email" name="email" type="email" value={this.props.contact.email} maxLength="30" required onChange={this.onChange} className="form-control" />
+				</div>
+				<div className="form-group">
+					<label forHtml="phone">Phone</label>
+					<input ref="phone" name="phone" type="tel" value={this.props.contact.phone} maxLength="15" onChange={this.onChange} className="form-control" />
+				</div>
+				<div className="form-group">
+					<label forHtml="address">Address</label>
+					<input ref="address" name="address" type="text" value={this.props.contact.address} maxLength="50" onChange={this.onChange} className="form-control" />
+				</div>
+                <button type="submit" className="btn btn-primary">{this.props.contact.id ? "Save" : "Add"}</button>&nbsp;
+					{this.props.contact.id ? <button onClick={this.props.handleDeleteClick } className="btn btn-danger">Delete</button> : null}&nbsp;
+					{this.props.contact.id ? <button onClick={this.props.handleCancelClick } className="btn btn-default">Cancel</button> : null}
+					{this.props.message ? <div>{this.props.message}</div> : null}
+			</form>
         );
     },
     onChange: function() {
@@ -97,27 +118,36 @@ var ContactPanel = React.createClass({
         };
     },
     render: function() {
-        return(
-            <div className="row">
-                <div className="one-half column">
-                    <SearchPanel
-                        search={this.state.search}
-                        onSearchChanged={this.onSearchChanged}
-                        onClearSearch={this.onClearSearch}
-                    />
-                    <ContactTable contacts={this.state.contacts} handleEditClickPanel={this.handleEditClickPanel} />
-                </div>
-                <div className="one-half column">
-                    <ContactForm 
-                        contact={this.state.editingContact} 
-                        message={this.state.message} 
-                        handleChange={this.handleChange}
-                        handleSubmitClick={this.handleSubmitClick}
-                        handleCancelClick={this.handleCancelClick}
-                        handleDeleteClick={this.handleDeleteClick}
-                    />
-                </div>
-            </div>
+    	return (
+			<div className="container-fluid">
+				<h3 className="text-primary text-center">React Address Book</h3>
+				<div className="row">
+					<div className="col-md-8">
+						<div className="panel panel-info">
+							<div className="panel-heading"><span className="glyphicon glyphicon-th-list" aria-hidden="true"></span></div>
+							<div className="panel-body">
+								<SearchPanel search={this.state.search}
+											 onSearchChanged={this.onSearchChanged}
+											 onClearSearch={this.onClearSearch} />
+								<ContactTable contacts={this.state.contacts} handleEditClickPanel={this.handleEditClickPanel} />
+							</div>
+						</div>
+					</div>
+					<div className="col-md-4">
+						<div className="panel panel-success">
+							<div className="panel-heading"><span className="glyphicon glyphicon-user" aria-hidden="true"></span></div>
+							<div className="panel-body">
+								<ContactForm contact={this.state.editingContact}
+										 message={this.state.message}
+										 handleChange={this.handleChange}
+										 handleSubmitClick={this.handleSubmitClick}
+										 handleCancelClick={this.handleCancelClick}
+										 handleDeleteClick={this.handleDeleteClick} />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
         );
     },
     componentDidMount: function() {
